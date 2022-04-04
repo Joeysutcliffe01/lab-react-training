c513a89a785199c1060bdc687cb0133568d1712a
Joey Sutcliffe <joeysutcliffe0@gmail.comgit config --global user.email joeysutcliffe0@gmail.com>
2022-04-04 16:57:50 +0200

    itteration 1

--- src/App.js
+++ src/App.js
@@ -4,22 +4,37 @@ import { IdCard } from './components/IdCard';
 function App() {
   return (
     <div className="App">
-      <IdCard
-        lastName="Doe"
-        // firstName="John"
-        // gender="male"
-        // height={178}
-        // birth={new Date('1992-07-14')}
-        // picture="https://randomuser.me/api/portraits/men/44.jpg"
-      />
-      {/* <IdCard
-        lastName="Delores "
-        firstName="Obrien"
-        gender="female"
-        height={172}
-        // birth={new Date('1988-05-11')}
-        picture="https://randomuser.me/api/portraits/women/44.jpg"
-      /> */}
+      <div className="card">
+        <IdCard
+          lastName="Doe"
+          firstName="John"
+          gender="male"
+          height={178}
+          birth={new Date('1992-07-14')}
+          picture={
+            <img
+              src="https://randomuser.me/api/portraits/men/44.jpg"
+              alt="pic"
+            />
+          }
+        />
+      </div>
+
+      <div className="card">
+        <IdCard
+          lastName="Delores "
+          firstName="Obrien"
+          gender="female"
+          height={172}
+          birth={new Date('1988-05-11')}
+          picture={
+            <img
+              src="https://randomuser.me/api/portraits/women/44.jpg"
+              alt="pic"
+            />
+          }
+        />
+      </div>
     </div>
   );
 }
--- src/components/IdCard.js
+++ src/components/IdCard.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
-function IdCard({ lastName, firstName, gender, height, birth, picture }) {
+export function IdCard({
+  firstName,
+  lastName,
+  gender,
+  height,
+  birth,
+  picture,
+}) {
+  console.log(birth.toDateString());
+  const birthDate = birth.toDateString();
   return (
-      <div>
-      {console.log(lastName)}
-      <h3>lastName: {lastName}</h3>
-      <h3>firstName: {firstName}</h3>
-      <h3>gender: {gender}</h3>
-      <h3>height: {height}</h3>
-      {/* <h3>birth: {birth}</h3> */}
-      <h3>picture: {picture}</h3>
-  </div>;
-  )
+    <div className="inner-container">
+      <div className="info">
+        <h3>firstName: {firstName}</h3>
+        <h3>lastName: {lastName}</h3>
+        <h3>gender: {gender}</h3>
+        <h3>height: {height}</h3>
+      </div>
+      <h3>birth: {birthDate}</h3>
+      <div className="pic-container">
+        <h3>picture: {picture}</h3>
+      </div>
+    </div>
+  );
 }
-
-export default IdCard;
